c23485d327304695c02875078711d994477b8819
Martin Hansen Pedersen <mart103j@edu.campusvejle.dk>
2019-09-25 09:13:23 +0200

    BIZ v.1

---
+++ BIZ/ClassLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIZ
+{
+    public class ClassLogin
+    {
+        private string _id;
+        private string _user;
+
+        public ClassLogin()
+        {
+
+        }
+
+        public string id
+        {
+            get { return _id; }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                }
+            }
+        }
+
+        public string user
+        {
+            get { return _user; }
+            set
+            {
+                if (_user != value)
+                {
+                    _user = value;
+                }
+            }
+        }
+
+        public ClassPerson GetUserData(int id, string user)
+        {
+            ClassPerson person = new ClassPerson();
+
+            return person;
+        }
+    }
+}
